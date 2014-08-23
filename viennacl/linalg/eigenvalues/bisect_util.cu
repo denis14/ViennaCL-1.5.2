5cebef603b26c40d98f603bf487c239b3b76f79e
w2z43t5 <w2z43t5@users.noreply.github.com>
2014-08-23 22:58:13 +0200

    Update bisect_util.cu

--- viennacl/linalg/eigenvalues/bisect_util.cu
+++ viennacl/linalg/eigenvalues/bisect_util.cu
@@ -315,7 +315,7 @@ createIndicesCompaction(T *s_compaction_list_exc,
             unsigned int  ai = offset*(2*tid+1)-1;
             unsigned int  bi = offset*(2*tid+2)-1;
             unsigned short temp = s_compaction_list_exc[bi];
-            if(bi < 65)
+            if(bi < 130)
             {
                s_compaction_list_exc[bi] =   temp + s_compaction_list_exc[ai];
  
