a39351d64732d87b936580831918288a94eacce4
w2z43t5 <w2z43t5@users.noreply.github.com>
2014-08-24 22:32:10 +0200

    Update bisect_util.cu

--- viennacl/linalg/eigenvalues/bisect_util.cu
+++ viennacl/linalg/eigenvalues/bisect_util.cu
@@ -314,12 +314,11 @@ createIndicesCompaction(T *s_compaction_list_exc,
 
             unsigned int  ai = offset*(2*tid+1)-1;
             unsigned int  bi = offset*(2*tid+2)-1;
-            unsigned short temp = s_compaction_list_exc[bi];
-            __syncthreads();
-            s_compaction_list_exc[bi] =   temp + s_compaction_list_exc[ai];
+            //unsigned short temp = s_compaction_list_exc[bi];
+           // __syncthreads();
+           // s_compaction_list_exc[bi] =   temp + s_compaction_list_exc[ai];
     
-           //s_compaction_list_exc[bi] =   s_compaction_list_exc[bi]
-             //                          + s_compaction_list_exc[ai];
+           s_compaction_list_exc[bi] =   s_compaction_list_exc[bi] + s_compaction_list_exc[ai];
              
             if(s_compaction_list_exc[bi] > 512)
             {
@@ -345,12 +344,11 @@ createIndicesCompaction(T *s_compaction_list_exc,
 
             unsigned int  ai = offset*(tid+1) - 1;
             unsigned int  bi = ai + (offset >> 1);
-            unsigned short temp = s_compaction_list_exc[bi];
-            __syncthreads();
-            s_compaction_list_exc[bi] =   temp + s_compaction_list_exc[ai];
+            //unsigned short temp = s_compaction_list_exc[bi];
+            //__syncthreads();
+            //s_compaction_list_exc[bi] =   temp + s_compaction_list_exc[ai];
  
-              // s_compaction_list_exc[bi] =   s_compaction_list_exc[bi]
-                //                           + s_compaction_list_exc[ai];
+             s_compaction_list_exc[bi] =   s_compaction_list_exc[bi] + s_compaction_list_exc[ai];
 
              
             if(s_compaction_list_exc[bi] > 512)
