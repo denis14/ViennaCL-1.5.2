a3bfc4a246bc7e3ad014f32cad672b03611ed5d2
w2z43t5 <w2z43t5@users.noreply.github.com>
2014-08-25 15:03:40 +0200

    Update bisect_large.hpp

--- viennacl/linalg/eigenvalues/bisect_large.hpp
+++ viennacl/linalg/eigenvalues/bisect_large.hpp
@@ -351,7 +351,7 @@ processResultDataLargeMatrix(const InputData &input, ResultDataLarge &result,
         eigenvalues[pos_one[i] - 1] = left_one[i];
     }
 
-    for( unsigned int i = 0; i < (mat_size < 15 ? mat_size : 15); ++i)
+    for( unsigned int i = 0; i < (mat_size < 200 ? mat_size : 200); ++i)
       std::cout << "Eigenvalue " << i << "= " << eigenvalues[i] << std::endl;
 
     if (0 == user_defined)
