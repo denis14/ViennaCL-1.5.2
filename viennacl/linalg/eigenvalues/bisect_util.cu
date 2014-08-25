8b4bf48631d68790882077b4314edf3079e39e60
w2z43t5 <w2z43t5@users.noreply.github.com>
2014-08-25 22:24:39 +0200

    Update bisect_util.cu

--- viennacl/linalg/eigenvalues/bisect_util.cu
+++ viennacl/linalg/eigenvalues/bisect_util.cu
@@ -404,9 +404,8 @@ compactIntervals(float *s_left, float *s_right,
     {
         unsigned int addr_w = num_threads_active + s_compaction_list[tid];
         //printf("num_thread_actice: %i \n", num_threads_active);
-        if(s_compaction_list[tid] > 512)
-          printf(" !!!! s_comp_list[%i]: %i \n", tid, s_compaction_list[tid]);
-        //printf("addr_w = %i\n", addr_w);
+        if(addr_w > 511)
+          printf("addr_w = %u\n", addr_w);
         s_left[addr_w] = mid;
         s_right[addr_w] = right;
         s_left_count[addr_w] = mid_count;
