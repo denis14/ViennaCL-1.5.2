c667fea75d93c45461c30f2867e5e5b063332fb0
w2z43t5 <w2z43t5@users.noreply.github.com>
2014-08-26 17:56:54 +0200

    Update bisect_kernel_large.cuh

--- viennacl/linalg/eigenvalues/bisect_kernel_large.cuh
+++ viennacl/linalg/eigenvalues/bisect_kernel_large.cuh
@@ -230,7 +230,8 @@ bisectKernelLarge(float *g_d, float *g_s, const unsigned int n,
 
     // for all active threads read intervals from the last level
     // the number of (worst case) active threads per level l is 2^l
-
+     for(unsigned int i = 0; i < 5; ++i)
+     {
         subdivideActiveInterval(tid, s_left, s_right, s_left_count, s_right_count,
                                 num_threads_active,
                                 left, right, left_count, right_count,
@@ -329,6 +330,7 @@ bisectKernelLarge(float *g_d, float *g_s, const unsigned int n,
 
 
     __syncthreads();
+    }
 /*
     // generate two lists of intervals; one with intervals that contain one
     // eigenvalue (or are converged), and one with intervals that need further
