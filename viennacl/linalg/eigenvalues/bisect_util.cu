cd0d606974509e428a706bb8c90c784d352c8573
w2z43t5 <w2z43t5@users.noreply.github.com>
2014-09-01 13:45:57 +0200

    Update bisect_util.cu

--- viennacl/linalg/eigenvalues/bisect_util.cu
+++ viennacl/linalg/eigenvalues/bisect_util.cu
@@ -478,8 +478,8 @@ subdivideActiveInterval(const unsigned int tid,
         right_count = s_right_count[tid];
 
         // check if thread already converged
-        if( std::abs(left - right) > 0.00000001f )
-        //if (left != right)
+        //if( std::abs(left - right) > 0.00000001f )
+        if (left != right)
         {
 
             mid = computeMidpoint(left, right);
