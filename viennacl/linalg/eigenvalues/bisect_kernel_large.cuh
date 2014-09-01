1e63ca81ba142d1824dc3577a41fd8a0a23f4bf0
w2z43t5 <w2z43t5@users.noreply.github.com>
2014-09-01 17:46:45 +0200

    Update bisect_kernel_large.cuh

--- viennacl/linalg/eigenvalues/bisect_kernel_large.cuh
+++ viennacl/linalg/eigenvalues/bisect_kernel_large.cuh
@@ -668,7 +668,7 @@ compactStreamsFinal(const unsigned int tid, const unsigned int tid_2,
 
     // store compactly in shared mem
     s_left[ptr_w] = left;
-    if( s_left[ptw_w] == 2.5 && ptw_w == 21 )
+    if( s_left[ptr_w] == 2.5 && ptr_w == 21 )
       printf("s_left[21] = 2.5!!!\n");
     s_right[ptr_w] = right;
     s_left_count[ptr_w] = left_count;
