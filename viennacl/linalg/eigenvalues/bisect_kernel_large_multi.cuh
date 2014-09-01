5e7cf6d1d058e89fc137f2f44d3592605633a9f6
w2z43t5 <w2z43t5@users.noreply.github.com>
2014-09-01 12:24:18 +0200

    Update bisect_kernel_large_multi.cuh

--- viennacl/linalg/eigenvalues/bisect_kernel_large_multi.cuh
+++ viennacl/linalg/eigenvalues/bisect_kernel_large_multi.cuh
@@ -181,10 +181,10 @@ bisectKernelLarge_MultIntervals(float *g_d, float *g_s, const unsigned int n,
         if (tid < num_threads_active)
         {
             
-            if( s_right_count[tid] < s_left_count[tid] )
+            /*if( s_right_count[tid] < s_left_count[tid] )
             {
               printf("3: left_count = %u\t right_count = %u \t tid = %u \n", left_count, right_count, tid);
-            }
+            }*/
             
             // store intervals
             if (left != right)
