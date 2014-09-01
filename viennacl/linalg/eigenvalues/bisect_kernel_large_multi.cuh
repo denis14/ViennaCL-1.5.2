62f18f6481c652cd20d17aa6eea2f8ef0a1524c8
w2z43t5 <w2z43t5@users.noreply.github.com>
2014-09-01 11:59:26 +0200

    Update bisect_kernel_large_multi.cuh

--- viennacl/linalg/eigenvalues/bisect_kernel_large_multi.cuh
+++ viennacl/linalg/eigenvalues/bisect_kernel_large_multi.cuh
@@ -129,7 +129,7 @@ bisectKernelLarge_MultIntervals(float *g_d, float *g_s, const unsigned int n,
         s_right[tid] = g_right[c_block_start + tid];
         s_left_count[tid]  = g_left_count[c_block_start + tid];
         s_right_count[tid] = g_right_count[c_block_start + tid];
-        printf("1: tid = %u s_r = %u \t s_l = %u \t num_threads_active: %u\n", tid, s_right[tid], s_left[tid], num_threads_active);       // selbst hinzugefuegt
+        printf("1: tid = %u s_r = %f \t s_l = %f \t num_threads_active: %u\n", tid, s_right[tid], s_left[tid], num_threads_active);       // selbst hinzugefuegt
 
         
     }
