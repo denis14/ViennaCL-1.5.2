fbefe0f9c8b59c9130e1ca26614d1d5aeb13e366
w2z43t5 <w2z43t5@users.noreply.github.com>
2014-09-02 13:05:52 +0200

    Update structs.hpp

--- viennacl/linalg/eigenvalues/structs.hpp
+++ viennacl/linalg/eigenvalues/structs.hpp
@@ -69,8 +69,8 @@ class InputData
              //std_a[i] = i % 11 + 4;
              //std_b_raw[i] = i % 9 + 2;
              
-             a[i] = ((float)(i % 19)) - 4.5f;
-             b[i] = ((float)(i % 15)) - 4.5f;
+             a[i] = ((float)(i % 12)) - 4.5f;
+             b[i] = ((float)(i % 11)) - 4.5f;
 
            }
            
