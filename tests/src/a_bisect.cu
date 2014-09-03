b725af381f12b759616fb883804dadf6fc77ab93
w2z43t5 <w2z43t5@users.noreply.github.com>
2014-09-03 12:31:47 +0200

    Update a_bisect.cu

--- tests/src/a_bisect.cu
+++ tests/src/a_bisect.cu
@@ -1,142 +1,130 @@
+/* =========================================================================
+   Copyright (c) 2010-2014, Institute for Microelectronics,
+                            Institute for Analysis and Scientific Computing,
+                            TU Wien.
+   Portions of this software are copyright by UChicago Argonne, LLC.
+
+                            -----------------
+                  ViennaCL - The Vienna Computing Library
+                            -----------------
+
+   Project Head:    Karl Rupp                   rupp@iue.tuwien.ac.at
+
+   (A list of authors and contributors can be found in the PDF manual)
+
+   License:         MIT (X11), see file LICENSE in the base directory
+============================================================================= */
+
 /*
- * Copyright 1993-2014 NVIDIA Corporation.  All rights reserved.
- *
- * Please refer to the NVIDIA end user license agreement (EULA) associated
- * with this source code for terms and conditions that govern your use of
- * this software. Any use, reproduction, disclosure, or distribution of
- * this software and related documentation outside the terms of the EULA
- * is strictly prohibited.
- *
- */
+*
+*   Test file for tql-algorithm
+*
+*/
 
-/* Computation of eigenvalues of symmetric, tridiagonal matrix using
- * bisection.
- */
+// include necessary system headers
+#include <iostream>
 
-// includes, system
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <float.h>
-#include <assert.h>
+#ifndef NDEBUG
+  #define NDEBUG
+#endif
 
-// includes, project
-//#include <helper_functions.h>
-//#include <helper_cuda.h>
+#define VIENNACL_WITH_UBLAS
 
+//include basic scalar and vector types of ViennaCL
 #include "viennacl/scalar.hpp"
 #include "viennacl/vector.hpp"
-#include "viennacl/matrix.hpp"
-#include "viennacl/compressed_matrix.hpp"
 
-#include "viennacl/linalg/eigenvalues/config.hpp"
-#include "viennacl/linalg/eigenvalues/structs.hpp"
-#include "viennacl/linalg/eigenvalues/matlab.hpp"
-#include "viennacl/linalg/eigenvalues/util.hpp"
-#include "viennacl/linalg/eigenvalues/gerschgorin.hpp"
-#include "viennacl/linalg/eigenvalues/bisect_large.hpp"
+#include "viennacl/linalg/qr-method.hpp"
 
-#include "viennacl/linalg/eigenvalues/bisect_large.cuh"
-
-////////////////////////////////////////////////////////////////////////////////
-// declaration, forward
-bool runTest(int argc, char **argv);
-
-////////////////////////////////////////////////////////////////////////////////
-// Program main
-////////////////////////////////////////////////////////////////////////////////
-int
-main(int argc, char **argv)
-{
-    bool bQAResults = false;
-
-    printf("Starting eigenvalues\n");
-
-    bQAResults = runTest(argc, argv);
-    printf("Test %s\n", bQAResults ? "Succeeded!" : "Failed!");
-
-    exit(bQAResults ? EXIT_SUCCESS : EXIT_FAILURE);
-}
+#define EPS 10.0e-5
 
 
 
-////////////////////////////////////////////////////////////////////////////////
-//! Run a simple test for CUDA
-////////////////////////////////////////////////////////////////////////////////
-bool
-runTest(int argc, char **argv)
-{
-    bool bCompareResult = false;
-    // default
-    unsigned int mat_size = 70520;
-    // flag if the matrix size is due to explicit user request
-    unsigned int user_defined = 0;
-    // desired precision of eigenvalues
-    float  precision = 0.00001f;
-    unsigned int iters_timing = 1;
-    char  *result_file = "eigenvalues.dat";
-
-    {
-    // set up input
-    InputData input(argv[0], mat_size, user_defined);
-
-    // compute Gerschgorin interval
-    float lg = FLT_MAX;
-    float ug = -FLT_MAX;
-    computeGerschgorin(input.a, input.b + 1, mat_size, lg, ug);
-    //computeGerschgorin(input.std_a, input.std_b_raw, mat_size, lg, ug);
-    printf("Gerschgorin interval: %f / %f\n", lg, ug);
+namespace ublas = boost::numeric::ublas;
+typedef float     ScalarType;
 
 
-    // initialize memory for result
-    ResultDataLarge result(mat_size);
-    std::cout << "initResultDataLargeMatrix" << std::endl;
-    initResultDataLargeMatrix(result, mat_size);
+// Test the eigenvectors
+// Perform the multiplication (T - lambda * I) * Q, with the original tridiagonal matrx T, the
+// eigenvalues lambda and the eigenvectors in Q. Result has to be 0.
+
 /*
-    // run the kernel
-    computeEigenvaluesLargeMatrix(input, result, mat_size,
-                                  precision, lg, ug,
-                                  iters_timing);
+template <typename MatrixLayout>
+bool test_eigen_val_vec(viennacl::matrix<ScalarType, MatrixLayout> & Q,
+                      ublas::vector<ScalarType> & eigenvalues,
+                      ublas::vector<ScalarType> & d,
+                      ublas::vector<ScalarType> & e)
+{
+  unsigned int Q_size = Q.size2();
+  ScalarType value = 0;
 
-    // get the result from the device and do some sanity checks
-    // save the result if user specified matrix size
-    bCompareResult = processResultDataLargeMatrix(input, result, mat_size, result_file,
-                                                  user_defined, argv[0]);
+  for(unsigned int j = 0; j < Q_size; j++)
+  {
+    // calculate first row
+    value = (d[0]- eigenvalues[j]) * Q(0, j) + e[1] * Q(1, j);
+    if(value > EPS)
+      return false;
 
-*/
-    dim3 blocks(1, 1, 1);
-    dim3 threads(256, 1, 1);
-    std::cout << "Start bisectKernelLarge\t" << std::endl;
-            bisectKernelLarge<<< blocks, threads >>>
-            (input.g_a, input.g_b, mat_size,
-              lg, ug, 0, mat_size, precision,
-             result.g_num_one, result.g_num_blocks_mult,
-             result.g_left_one, result.g_right_one, result.g_pos_one,
-             result.g_left_mult, result.g_right_mult,
-             result.g_left_count_mult, result.g_right_count_mult,
-             result.g_blocks_mult, result.g_blocks_mult_sum
-            );
-
-    viennacl::linalg::cuda::VIENNACL_CUDA_LAST_ERROR_CHECK("Kernel launch failed.");
-    checkCudaErrors(cudaDeviceSynchronize());
-    
-    // cleanup
-    std::cout << "CleanupResultDataLargeMatrix!" << std::endl;
-    cleanupResultDataLargeMatrix(result);
-
-    std::cout << "cleanupInputData" << std::endl;
-    input.cleanupInputData();
+    // calcuate inner rows
+    for(unsigned int i = 1; i < Q_size - 1; i++)
+    {
+        value = e[i] * Q(i - 1, j) + (d[i]- eigenvalues[j]) * Q(i, j) + e[i + 1] * Q(i + 1, j);
+        if(value > EPS)
+          return false;
     }
-    // cudaDeviceReset causes the driver to clean up all state. While
-    // not mandatory in normal operation, it is good practice.  It is also
-    // needed to ensure correct operation when the application is being
-    // profiled. Calling cudaDeviceReset causes all profile data to be
-    // flushed before the application exits
-    
-    std::cout << "cudaDeviceReset" << std::endl;
-    cudaDeviceReset();
 
-    return bCompareResult;
+    // calculate last row
+    value = e[Q_size - 1] * Q(Q_size - 2, j) + (d[Q_size - 1] - eigenvalues[j]) * Q(Q_size - 1, j);
+     if(value > EPS)
+       return false;
+  }
+  return true;
+}
+*/
+
+/**
+ * Test the tql2 algorithm for symmetric tridiagonal matrices.
+ */
+
+template <typename MatrixLayout>
+void test_qr_method_sym()
+{
+  std::size_t sz = 10;
+
+  viennacl::matrix<ScalarType, MatrixLayout> Q = viennacl::identity_matrix<ScalarType>(sz);
+  ublas::vector<ScalarType> d(sz), e(sz), d_ref(sz), e_ref(sz); //d is major diagonal, e is minor diagonal
+
+  std::cout << "Testing matrix of size " << sz << "-by-" << sz << std::endl << std::endl;
+
+  for(unsigned int i = 0; i < sz; ++i)
+  {
+    //std_a[i] = i % 11 + 4;
+    //std_b_raw[i] = i % 9 + 2;
+    d[i] = ((float)(i % 9)) - 4.5f;
+    e[i] = ((float)(i % 5)) - 4.5f;
+  }
+  e[0] = 0.0f;
+  d_ref = d;
+  e_ref = e;
+
+//--------------------------------------------------------
+  viennacl::linalg::tql2(Q, d, e);
+
+ // if(!test_eigen_val_vec<MatrixLayout>(Q, d, d_ref, e_ref))
+   //  exit(EXIT_FAILURE);
+
+  for( unsigned int i = 0; i < sz; ++i)
+    std::cout << "Eigenvalue " << i << "= " << d[i] << std::endl;
 }
 
+int main()
+{
+
+  std::cout << std::endl << "Testing tql2 algorithm for symmetric tridiagonal row-major matrices..." << std::endl;
+  test_qr_method_sym<viennacl::row_major>();
+/*
+  std::cout << std::endl << "Testing QL algorithm for symmetric tridiagonal column-major matrices..." << std::endl;
+  test_qr_method_sym<viennacl::column_major>();
+*/
+  std::cout << std::endl <<"--------TEST SUCCESSFULLY COMPLETED----------" << std::endl;
+}
