ccfd38188aed327152f47e31a472c243fd1d6094
Denis <denis.ojdanic@yahoo.com>
2014-09-03 14:55:26 +0200

    Update a_bisect.cu

--- tests/src/a_bisect.cu
+++ tests/src/a_bisect.cu
@@ -254,6 +254,7 @@ runTest(int argc, char **argv)
   {
      if(std::abs(result.std_eigenvalues[(mat_size - 1) - i] - d[i]) < EPS)
 	  return EXIT_FAILURE;
+	  std::cout << i;
   }
                                                   
                                                   
