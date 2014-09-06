67ac47ae2fcf922cc8c5cb474d379a73d581fec6
w2z43t5 <w2z43t5@users.noreply.github.com>
2014-09-06 10:06:49 +0200

    Update a_bisect_mult.cu

--- tests/src/a_bisect_mult.cu
+++ tests/src/a_bisect_mult.cu
@@ -69,7 +69,7 @@ runTest(int argc, char **argv)
 {
     bool bCompareResult = false;
     // default
-    unsigned int mat_size = 1500;
+    unsigned int mat_size = 15000;
     // flag if the matrix size is due to explicit user request
     unsigned int user_defined = 0;
     // desired precision of eigenvalues
