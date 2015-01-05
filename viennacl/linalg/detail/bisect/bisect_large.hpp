b8ed2e479bbacd31fb12521f5be5caf9ef40ccd1
Andi <andreasselinger@gmail.com>
2015-01-05 20:46:32 +0100

    ...

--- viennacl/linalg/detail/bisect/bisect_large.hpp
+++ viennacl/linalg/detail/bisect/bisect_large.hpp
@@ -75,23 +75,23 @@ computeEigenvaluesLargeMatrix(InputData<NumericT> &input, ResultDataLarge<Numeri
   // First kernel call: decide on which intervals bisect_Large_OneIntervals/
   // bisect_Large_MultIntervals is executed
   viennacl::linalg::detail::bisectLarge(input, result, mat_size, lg, ug, precision);
-  viennacl::backend::finish();
-  times[1] = timer.get() * 1000.;
+ // viennacl::backend::finish();
+ // times[1] = timer.get() * 1000.;
 
 
   // compute eigenvalues for intervals that contained only one eigenvalue
   // after the first processing step
   timer.start();
   viennacl::linalg::detail::bisectLarge_OneIntervals(input, result, mat_size, precision);
-  viennacl::backend::finish();
-  times[2] = timer.get() * 1000.;
+ // viennacl::backend::finish();
+ // times[2] = timer.get() * 1000.;
 
   // process intervals that contained more than one eigenvalue after
   // the first processing step
   timer.start();
   viennacl::linalg::detail::bisectLarge_MultIntervals(input, result, mat_size, precision);
-  viennacl::backend::finish();
-  times[3] = timer.get() * 1000.;
+//  viennacl::backend::finish();
+//  times[3] = timer.get() * 1000.;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -154,8 +154,8 @@ processResultDataLargeMatrix(ResultDataLarge<NumericT> &result,
     {
         result.std_eigenvalues[pos_one[i] - 1] = left_one[i];
     }
-    viennacl::backend::finish();
-    times[0] = timer_pp.get() * 1000;
+  //  viennacl::backend::finish();
+  //  times[0] = timer_pp.get() * 1000;
     return bCompareResult;
 }
 } // namespace detail
